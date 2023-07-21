298c19e017bb402cd66df96e110dfdd83d101c44
Nate <torN8o.sycro@gmail.com>
2023-07-20 23:09:24 -0500

    made buttons nice

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    cssModules: {
+        folders: {
+          styles: {
+            global: true,
+          },
+        },
+      },
+    };
--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,14 @@ export default function Home() {
     return (
         <>
         <Head>
-            <title>Vexaro | Software and Web Design</title>
+            <title>Spirge | Software and Web Design</title>
             <meta 
                 property="og:title" 
                 content="Spirge | Software and Web Design" />
             <meta property="og:type" content="website" />
             <meta name="description" content="Spirge is a machine learning software distribution platform and web design company started by teen developers Nathaniel Sun and Davis Miller." />
         </Head>
-        <section>
+        <body className={`${homestyle.full_env}`}>
             <div className={`${homestyle.d_encompass} ${genstyle.div}`}>            
                 <div className={`${homestyle.d_top} ${genstyle.div}`}>
                     <Link href='#'>
@@ -28,49 +28,49 @@ export default function Home() {
                     </Link>
                 </div>
                 <div className={`${homestyle.d_objects} ${genstyle.div}`}>
-                    <div className={`${homestyle.d_leftnodes}`}>
+                    <div className={`${homestyle.d_sidenodes}`}>
                         <div className={`${homestyle.l_nodes} ${genstyle.div}`}>
-                            <div className={`${homestyle.node_l} ${genstyle.div}`}></div>
-                            <div className={`${homestyle.node_l} ${genstyle.div}`}></div>
-                            <div className={`${homestyle.node_l} ${genstyle.div}`}></div>
-                            <div className={`${homestyle.node_l} ${genstyle.div}`}></div>
-                            <div className={`${homestyle.node_l} ${genstyle.div}`}></div>
+                            <div className={`${homestyle.node} ${genstyle.div}`}></div>
+                            <div className={`${homestyle.node} ${genstyle.div}`}></div>
+                            <div className={`${homestyle.node} ${genstyle.div}`}></div>
+                            <div className={`${homestyle.node} ${genstyle.div}`}></div>
+                            <div className={`${homestyle.node} ${genstyle.div}`}></div>
                         </div>
                     </div>
                     <div className={`${homestyle.d_mainsign} ${genstyle.div}`}>
                         <div className={`${homestyle.sign} ${genstyle.div}`}>
-                            <h1 className={`${genstyle.h1}`}>SPIRGE SOFTWARE</h1>
+                            <h1 className={`${homestyle.h_sign} ${genstyle.h1}`}>SPIRGE SOFTWARE</h1>
                         </div>
                         <div className={`${homestyle.signsub} ${genstyle.div}`}>
-                            <h3 className={`${genstyle.h3}`}>Web Development and Machine Learning</h3>
+                            <h3 className={`${homestyle.h_signsub} ${genstyle.h3}`}>Web Development and Machine Learning</h3>
                         </div>
                     </div>
                     <div className={`${homestyle.d_midnodes} ${genstyle.div}`}></div>
                     <div className={`${homestyle.d_mainlist} ${genstyle.div}`}>
                         <div className={`${homestyle.d_list} ${genstyle.div}`}>
                             <ul className={`${homestyle.list}`}>
-                                <li><Link href="/main/about">ABOUT</Link></li>
-                                <li><Link href="/main/products">PRODUCTS</Link></li>
-                                <li><Link href="/main/examples">EXAMPLES</Link></li>
-                                <li><Link href="/main/projects">PROJECTS</Link></li>
-                                <li><Link href="/main/contact">CONTACT</Link></li>
+                                <li className={`${homestyle.li_buttons}`}><Link className={`${homestyle.link_buttons} ${genstyle.link}`} href="/main/about">ABOUT</Link></li>
+                                <li className={`${homestyle.li_buttons}`}><Link className={`${homestyle.link_buttons} ${genstyle.link}`} href="/main/products">PRODUCTS</Link></li>
+                                <li className={`${homestyle.li_buttons}`}><Link className={`${homestyle.link_buttons} ${genstyle.link}`} href="/main/examples">EXAMPLES</Link></li>
+                                <li className={`${homestyle.li_buttons}`}><Link className={`${homestyle.link_buttons} ${genstyle.link}`} href="/main/projects">PROJECTS</Link></li>
+                                <li className={`${homestyle.li_buttons}`}><Link className={`${homestyle.link_buttons} ${genstyle.link}`} href="/main/contact">CONTACT</Link></li>
                             </ul>
                         </div>
 
                     </div>
-                    <div className={`${homestyle.d_rightnodes} ${genstyle.div}`}>
+                    <div className={`${homestyle.d_sidenodes} ${genstyle.div}`}>
                         <div className={`${homestyle.r_nodes} ${genstyle.div}`}>
-                            <div className={`${homestyle.node_r} ${genstyle.div}`}></div>
-                            <div className={`${homestyle.node_r} ${genstyle.div}`}></div>
-                            <div className={`${homestyle.node_r} ${genstyle.div}`}></div>
-                            <div className={`${homestyle.node_r} ${genstyle.div}`}></div>
-                            <div className={`${homestyle.node_r} ${genstyle.div}`}></div>
+                            <div className={`${homestyle.node} ${genstyle.div}`}></div>
+                            <div className={`${homestyle.node} ${genstyle.div}`}></div>
+                            <div className={`${homestyle.node} ${genstyle.div}`}></div>
+                            <div className={`${homestyle.node} ${genstyle.div}`}></div>
+                            <div className={`${homestyle.node} ${genstyle.div}`}></div>
                         </div>
                     </div>
                 </div>
                 <div className={`${homestyle.d_bottom} ${genstyle.div}`}></div>
             </div>
-        </section>
+        </body>
         </>
     )
 }
