c65cdf949f1df1946f1923f84c16dc4871bed36d
SYCRO-2727 <torN8o.sycro@gmail.com>
2023-08-13 21:22:19 -0500

    web scraper info

--- pages/products/web-scraping.js
+++ pages/products/web-scraping.js
@@ -1,7 +1,43 @@
+import Link from 'next/link'
+import Nodes from '../../components/nodes'
+import styles from '../../styles/pages/product-info.module.css'
+
 const WebScrape = () => {
     return (
-        <div></div>
+        <section className={styles.encompass}>
+        <div className={styles.div_title}><h1 className={styles.div_title_text}>Custom web development with NextJS</h1></div>
+        <div className={styles.div_info}>
+            <div className={styles.details}>
+                <h2 className={styles.h2}>Details</h2>
+                <p className={styles.p}>Spirge can meet your web scraping needs with custom-built python bots. We typically use BeautifulSoup and Selenium, but if more advanced modules are requested, we can integrate those.</p>
+                <p className={styles.p}>We can script bots for tasks of all sizes, from simple html downloading to live data mining, <Link href="/main/contact" className={styles.link}>contact</Link> us and tell us what you need!</p>
+                <p className={styles.p}>All of our scraping bots respect site captchas and robots.txt files. We also cannot scrape restricted or premium parts of sites.</p>
+                <p className={styles.p}>All web scraping bots come with a basic SQLite database storage system for scraped data. You can optionally request a different method of storage, i.e. CSV, JSON, TXT, or MySQL.</p>
+                <p className={styles.p}>When making an inquiry to us to order your web scraper, provide us with detailed information about the sites and information you would like to scrape, the format you would like the data in (if not SQLite), your user-agent name for the bot, and optionally any login or authentication information we may need. From there we can work out the rest through email or phone.</p>
+            </div>
+            <div className={styles.nodes}><Nodes direction={'horizontal'}/></div>
+            <div className={styles.details}>
+                <h2 className={styles.h2}>Pricing</h2>
+                <p className={styles.p}>We offer very low starting rates for custom python web-scraping bots! Here are some reference examples:</p>
+                <ul>
+                    <li className={styles.p}>Basic HTML scraping bot, basic parsing, straightforward extraction - starting $49</li>
+                    <li className={styles.p}>Intermediate HTML scraping bot, navigates complex sites, datatable extraction, image extraction (if allowed) - starting $199</li>
+                    <li className={styles.p}>Advanced scraping bot, built-in user authentication, pagination management, complex HTML parsing, advanced data management and storing - starting $399</li>
+                    <li className={styles.p}>Professional scraping solutions, HTML or Java GUI integration, multi-user authentication, 24/7 scraping, proxies, live SQL database management - starting $699</li>
+                </ul>
+            </div>
+        </div>
+    </section>
     )
 }
+
 WebScrape.needsLayout = true;
+WebScrape.title = "Spirge Products | Website built using WordPress template"
+WebScrape.metadata = (  
+    <meta property="og:title" content={WebScrape.title} />,
+    <meta property="og:description" content="Spirge product python web scraping bot and data" />,
+    <meta property="og:url" content="https://spirge.com/products/web-scraping" />,
+    <meta name="twitter:title" content={WebScrape.title} />,
+    <meta name="twitter:description" content="Spirge product python web scraping bot and data" />
+)
 export default WebScrape;
--- pages/products/wordpress-website.js
+++ pages/products/wordpress-website.js
@@ -11,15 +11,21 @@ const WordSite = () => {
                 <h2 className={styles.h2}>Details</h2>
                 <p className={styles.p}>If you're looking for a stylish and efficient website to bring your ideas to life, Spirge has you covered!</p>
                 <p className={styles.p}><Link href="https://wordpress.com/" className={styles.link}>WordPress</Link> is an awesome website production site offering customizable templates for quick website deployment.</p>
-                <p className={styles.p}>Using WordPress Spirge can provide you with an advanced template designed by top-level website engineers, tailored specifically to your liking!</p>
+                <p className={styles.p}>Using WordPress, Spirge can quickly provide you with an advanced template designed by teams of highly experienced website engineers, tailored specifically to your liking!</p>
+                <p className={styles.p}>With a WordPress template, all pressure to visualize your intricate website structure will quickly dissolve. All that work is already done for you! All you need to do is simply provide us with your template choice and the data to fill it with.</p>
+                <p className={styles.p}>After your initial consult with us, we will provide you with a list of WordPress templates to choose from that fit your requested website styling.</p>
             </div>
             <div className={styles.nodes}><Nodes direction={'horizontal'}/></div>
             <div className={styles.details}>
                 <h2 className={styles.h2}>Pricing</h2>
-                <p className={styles.p}>We offer very low starting rates for NextJS websites! Here are some price estimates to get you started:</p>
+                <p className={styles.p}>We offer very low starting rates for WordPress-based websites! For WordPrice premium templates, the price is added to your final quote with no extra surcharge. Here are some price estimates to get you started:</p>
                 <ul>
-
+                    <li className={styles.p}>Basic portfolio website, 1-3 pages - starting $99</li>
+                    <li className={styles.p}>Basic informational website, 2-5 pages - starting $199</li>
+                    <li className={styles.p}>Personal/business product or blog website, 5-20+ pages - starting $399</li>
+                    <li className={styles.p}>E-commerce or social media platform website - starting $699</li>
                 </ul>
+                <p className={styles.p}>Note: pricing will change based on amount of style customization requested</p>
             </div>
         </div>
     </section>
